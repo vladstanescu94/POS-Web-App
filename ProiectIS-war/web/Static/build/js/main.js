a12fe07bccccd711f96f149554d093176cab69a5
Vlad Stanescu <vladstanescu94@gmail.com>
2018-11-20 20:17:17 +0200

    remove old navbar design

--- ProiectIS-war/web/Static/build/js/main.js
+++
@@ -1,8 +0,0 @@
-let hamburger = document.querySelector('.hamburger');
-let hamburgerLines = document.querySelectorAll('.hamburger__line');
-
-hamburger.addEventListener('click', function () {
-    for (let i = 0; i < hamburgerLines.length; i++) {
-       hamburgerLines[i].classList.toggle('change'); 
-    }
-});
