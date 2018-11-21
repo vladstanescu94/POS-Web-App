8aa3f693b4f03b220c609d341eaf46dcdbf982f4
Vlad Stanescu <vladstanescu94@gmail.com>
2018-11-21 21:56:13 +0200

    add jquery and qty func

---
+++ ProiectIS-war/web/Static/build/js/main.js
@@ -0,0 +1,30 @@
+$('.btn-minus').on('click', function (e) {
+    e.preventDefault();
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+ 
+  $input.val(value);
+});
+
+$('.btn-plus').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+ 
+    $input.val(value);
+});
