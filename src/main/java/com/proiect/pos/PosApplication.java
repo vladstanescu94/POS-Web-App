94e1e42a091a890fb4db013fb55ce7f2f52387fc
Stefan Precup <psa9710@gmail.com>
2018-11-30 22:03:59 +0200

    Equivalent of Netbeans APP

--- src/main/java/com/proiect/pos/PosApplication.java
+++ src/main/java/com/proiect/pos/PosApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
@@ -12,4 +14,11 @@ public class PosApplication {
     public static void main(String[] args) {
         SpringApplication.run(PosApplication.class, args);
     }
+
+//    @Bean
+//    public LocalEntityManagerFactoryBean entityManagerFactory(){
+//        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+//        factoryBean.setPersistenceUnitName("cpJpaPu");
+//        return factoryBean;
+//    }
 }
---
+++ src/main/java/com/proiect/pos/Product.java
@@ -0,0 +1,149 @@
+package com.proiect.pos;
+
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author psa97
+ */
+@Entity
+@Table(name = "PRODUCT")
+@XmlRootElement
+@NamedQueries({
+        @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p")
+        , @NamedQuery(name = "Product.findById", query = "SELECT p FROM Product p WHERE p.id = :id")
+        , @NamedQuery(name = "Product.findByName", query = "SELECT p FROM Product p WHERE p.name = :name")
+        , @NamedQuery(name = "Product.findByStock", query = "SELECT p FROM Product p WHERE p.stock = :stock")
+        , @NamedQuery(name = "Product.findByPrice", query = "SELECT p FROM Product p WHERE p.price = :price")})
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "STOCK")
+    private int stock;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRICE")
+    private double price;
+    @Lob
+    @Column(name = "IMAGE")
+    private Serializable image;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "product")
+    private Collection<InvoiceItems> invoiceItemsCollection;
+
+    public Product() {
+    }
+
+    public Product(Long id) {
+        this.id = id;
+    }
+
+    public Product(Long id, String name, int stock, double price) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Serializable getImage() {
+        return image;
+    }
+
+    public void setImage(Serializable image) {
+        this.image = image;
+    }
+
+    @XmlTransient
+    public Collection<InvoiceItems> getInvoiceItemsCollection() {
+        return invoiceItemsCollection;
+    }
+
+    public void setInvoiceItemsCollection(Collection<InvoiceItems> invoiceItemsCollection) {
+        this.invoiceItemsCollection = invoiceItemsCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.pos.Product[ id=" + id + " ]";
+    }
+
+}
