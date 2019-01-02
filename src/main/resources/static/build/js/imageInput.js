9e8be3bd7cabed2e571b8fdbc1ce5a9a5c54c4e2
Vlad <vladstanescu94@gmail.com>
2019-01-02 16:03:22 +0200

    add register image select

---
+++ src/main/resources/static/build/js/imageInput.js
@@ -0,0 +1,17 @@
+var inputs = document.querySelectorAll( '.login__input-image' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		fileName = e.target.value.split( '\\' ).pop();
+
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
