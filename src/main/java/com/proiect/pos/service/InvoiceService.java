c8fdad181897eb01a2e77a167566dc9381425b2b
Stefan Precup <psa9710@gmail.com>
2019-01-02 12:04:37 +0200

    Add InvoiceService

--- src/main/java/com/proiect/pos/service/InvoiceService.java
+++ src/main/java/com/proiect/pos/service/InvoiceService.java
@@ -1,4 +1,19 @@
 package com.proiect.pos.service;
 
+import com.proiect.pos.model.Invoice;
+import com.proiect.pos.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("invoiceService")
 public class InvoiceService {
+
+    private InvoiceRepository invoiceRepository;
+
+    @Autowired
+    public InvoiceService(InvoiceRepository invoiceRepository){this.invoiceRepository=invoiceRepository;}
+
+    public void saveInvoice(Invoice invoice) {
+        invoiceRepository.save(invoice);
+    }
 }
